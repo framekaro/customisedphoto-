a52cb2127ef167dbc9224c0c99797b3a730507d1
framekaro <framekaro7@gmail.com>
2026-09-11 16:22:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
+const FRAMEKARO_CONFIG = {
+    businessName: "FrameKaro",
+    tagline: "आपकी यादें, खूबसूरत Frame में",
+    
+    currency: "₹",
 
+    whatsappNumber: "",
+
+    supportEmail: "",
+
+    deliveryMessage: "Home Delivery Available",
+
+    location: "Damoh, Madhya Pradesh"
+};
