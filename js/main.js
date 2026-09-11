57add2b26a4db9707b09fb3756ef8b1cbed832d8
framekaro <framekaro7@gmail.com>
2026-09-11 16:23:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const brandName = document.querySelectorAll("[data-business-name]");
+    const tagline = document.querySelectorAll("[data-tagline]");
+
+    brandName.forEach(element => {
+        element.textContent = FRAMEKARO_CONFIG.businessName;
+    });
+
+    tagline.forEach(element => {
+        element.textContent = FRAMEKARO_CONFIG.tagline;
+    });
+
+});
